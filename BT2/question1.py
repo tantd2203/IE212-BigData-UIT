525a904f150c4aa28ebc6d9a2cb165e200e4bffd
tantd9 <tranduytan.220300@gmail.com>
2024-03-11 22:49:24 +0700

    :whale:

--- BT2/question1.py
+++ BT2/question1.py
@@ -3,7 +3,6 @@ from pyspark.sql.functions import col, date_format
 from pyspark.sql import functions as F
 from pyspark.sql.types import DateType
 
-# Create SparkSession
 spark = SparkSession.builder.appName("TimestampConversion").getOrCreate()
 
 # Read movie and rating data
