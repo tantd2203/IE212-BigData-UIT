f63024b3d412d77fc5bb411cd61dcb67d60be26b
tantd9 <tranduytan.220300@gmail.com>
2024-05-09 12:31:58 +0700

    update

--- Kafka/consumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-import json
-import time
-producer = KafkaConsumer( bootstrap_servers='localhost:9092', maxbock_ms =5000)   
-
-producer.send('users_create', b'Hello, Kafka!')
